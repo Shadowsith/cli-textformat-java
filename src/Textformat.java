713ce0f1bf6ac7c9415c4b566cc6c98a1fa4ce6a
Philip Mayer <shadowsith94@gmail.com>
2018-11-25 14:32:38 +0100

    adding new features to Textformat class

--- src/Textformat.java
+++ src/Textformat.java
@@ -4,7 +4,7 @@ public class Textformat {
 
     private static String begin = "\033[";
     private static String end = "\033[0m";
-    private static String multiline_end = "\033[0a";
+    private static String multilineEnd = "\033[0a";
     private static String reset = "0m";
     private static String bold = "1m";
     private static String underline = "4m";
@@ -21,7 +21,7 @@ public class Textformat {
 
     public static String black(String text, boolean multiline) {
         if (multiline == true) 
-            return begin + FormatColor.Fg.black + text + multiline_end; 
+            return begin + FormatColor.Fg.black + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.black + text + end; 
     }
@@ -32,7 +32,7 @@ public class Textformat {
 
     public static String red(String text, boolean multiline) {
         if (multiline == true) 
-            return begin + FormatColor.Fg.red + text + multiline_end; 
+            return begin + FormatColor.Fg.red + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.red + text + end; 
     }
@@ -43,7 +43,7 @@ public class Textformat {
 
     public static String green(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.green + text + multiline_end; 
+            return begin + FormatColor.Fg.green + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.green + text + end; 
     }
@@ -54,7 +54,7 @@ public class Textformat {
 
     public static String yellow(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.yellow + text + multiline_end; 
+            return begin + FormatColor.Fg.yellow + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.yellow + text + end; 
     }
@@ -65,7 +65,7 @@ public class Textformat {
 
     public static String blue(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.blue + text + multiline_end; 
+            return begin + FormatColor.Fg.blue + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.blue + text + end; 
     }
@@ -76,7 +76,7 @@ public class Textformat {
 
     public static String magenta(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.magenta + text + multiline_end; 
+            return begin + FormatColor.Fg.magenta + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.magenta + text + end; 
     }
@@ -87,7 +87,7 @@ public class Textformat {
 
     public static String cyan(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.cyan + text + multiline_end; 
+            return begin + FormatColor.Fg.cyan + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.cyan + text + end; 
     }
@@ -98,109 +98,138 @@ public class Textformat {
 
     public static String white(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.white + text + multiline_end; 
+            return begin + FormatColor.Fg.white + text + multilineEnd; 
         else
             return begin + FormatColor.Fg.white + text + end; 
-    }
-
-    public static void main(String[] args) {
-        System.out.println(black("hi"));
     }
 
     // background
 
     public static String blackBg(String text) {
-        return begin + FormatColor.Fg.black + text + end;
+        return begin + FormatColor.Bg.black + text + end;
     }
 
     public static String blackBg(String text, boolean multiline) {
         if (multiline == true) 
-            return begin + FormatColor.Fg.black + text + multiline_end; 
+            return begin + FormatColor.Bg.black + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.black + text + end; 
+            return begin + FormatColor.Bg.black + text + end; 
     }
 
     public static String redBg(String text) {
-        return begin + FormatColor.Fg.red + text + end;
+        return begin + FormatColor.Bg.red + text + end;
     }
 
     public static String redBg(String text, boolean multiline) {
         if (multiline == true) 
-            return begin + FormatColor.Fg.red + text + multiline_end; 
+            return begin + FormatColor.Bg.red + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.red + text + end; 
+            return begin + FormatColor.Bg.red + text + end; 
     }
 
     public static String greenBg(String text) {
-        return begin + FormatColor.Fg.green + text + end;
+        return begin + FormatColor.Bg.green + text + end;
     }
 
     public static String greenBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.green + text + multiline_end; 
+            return begin + FormatColor.Bg.green + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.green + text + end; 
+            return begin + FormatColor.Bg.green + text + end; 
     }
 
     public static String yellowBg(String text) {
-        return begin + FormatColor.Fg.yellow + text + end;
+        return begin + FormatColor.Bg.yellow + text + end;
     }
 
     public static String yellowBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.yellow + text + multiline_end; 
+            return begin + FormatColor.Bg.yellow + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.yellow + text + end; 
+            return begin + FormatColor.Bg.yellow + text + end; 
     }
 
     public static String blueBg(String text) {
-        return begin + FormatColor.Fg.blue + text + end;
+        return begin + FormatColor.Bg.blue + text + end;
     }
 
     public static String blueBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.blue + text + multiline_end; 
+            return begin + FormatColor.Bg.blue + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.blue + text + end; 
+            return begin + FormatColor.Bg.blue + text + end; 
     }
 
     public static String magentaBg(String text) {
-        return begin + FormatColor.Fg.magenta + text + end;
+        return begin + FormatColor.Bg.magenta + text + end;
     }
 
     public static String magentaBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.magenta + text + multiline_end; 
+            return begin + FormatColor.Bg.magenta + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.magenta + text + end; 
+            return begin + FormatColor.Bg.magenta + text + end; 
     }
 
     public static String cyanBg(String text) {
-        return begin + FormatColor.Fg.cyan + text + end;
+        return begin + FormatColor.Bg.cyan + text + end;
     }
 
     public static String cyanBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.cyan + text + multiline_end; 
+            return begin + FormatColor.Bg.cyan + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.cyan + text + end; 
+            return begin + FormatColor.Bg.cyan + text + end; 
     }
 
     public static String whiteBg(String text) {
-        return begin + FormatColor.Fg.white + text + end;
+        return begin + FormatColor.Bg.white + text + end;
     }
 
     public static String whiteBg(String text, boolean multiline) {
         if (multiline == true)
-            return begin + FormatColor.Fg.white + text + multiline_end; 
+            return begin + FormatColor.Bg.white + text + multilineEnd; 
         else
-            return begin + FormatColor.Fg.white + text + end; 
+            return begin + FormatColor.Bg.white + text + end; 
     }
 
-    public static void main(String[] args) {
-        System.out.println(black("hi"));
+    // formatting
+    public static String bold(String text) {
+        return begin + bold + text + end; 
     }
 
+    public static String bold(String text, boolean multiline) {
+        if (multiline == true)
+            return begin + bold + text + multilineEnd; 
+        else
+            return begin + bold + text + end; 
+    }
+
+    public static String disableMultiline(String text, boolean multiline) {
+        return end; 
+    }
+
+    public static String inverse(String text) {
+        return begin + inverse + text + end; 
+    }
+
+    public static String inverse(String text, boolean multiline) {
+        if (multiline == true)
+            return begin + inverse + text + multilineEnd; 
+        else
+            return begin + inverse + text + end; 
+    }
+
+    public static String underline(String text) {
+        return begin + underline + text + end; 
+    }
+
+    public static String underline(String text, boolean multiline) {
+        if (multiline == true)
+            return begin + underline + text + multilineEnd; 
+        else
+            return begin + underline + text + end; 
+    }
 }
 
 // not named Color to avoid package conflicts
